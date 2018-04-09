72ade7d97502b9469cd1477036f6bd94a0107528
Zeeshan53 <kzishan287@gmail.com>
2018-04-09 16:01:47 +0500

    Task

--- ZeeshanAkbar/src/Assertion.java
+++ ZeeshanAkbar/src/Assertion.java
@@ -1,4 +1,14 @@
-
+import java.util.Scanner;
 public class Assertion {
 
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubimport java.util.Scanner;
+		
+			Scanner s = new Scanner(System.in);
+			
+			int age = s.nextInt();
+				assert  age <= 18:"IS not valid";
+					System.out.println("Age is "+age);
+		}
+	}
+
